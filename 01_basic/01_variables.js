68e23eef3f26895e3d28b71798982fb57fff8c5e
Anshu <155750493+Anshu039@users.noreply.github.com>
2025-02-19 15:04:59 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553 
+let accountEmail = "anshuss@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ludhiana"
+
+//accountId = 2 // not allowed
+
+accountEmail = "ac@gmail.com"
+accountPassword = "112233"
+accountCity = "Amritsar"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountId])
