55f4e661972d95407ab52926cc3e346fe848a3d9
Anshu <155750493+Anshu039@users.noreply.github.com>
2025-02-19 15:04:33 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let name = "Anshu";
+
+// alert(3 + 3)
+
+/* Data Types premitive
+integer
+bigint
+string
+boolean
+null
+undefined
+symbol
+*/
+
+/* object
+*/
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof name);
+
+
