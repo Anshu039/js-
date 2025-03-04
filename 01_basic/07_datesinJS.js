aeace8b17fedc88aa38eacd059d455bced439451
Anshu <155750493+Anshu039@users.noreply.github.com>
2025-03-04 15:52:11 +0000

    number

---
+++ 01_basic/07_datesinJS.js
@@ -0,0 +1,13 @@
+// Dates      
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+
+let mycreatedDate = new Date(2025, 0, 12)
+console.log(mycreatedDate.toDateString());
+
